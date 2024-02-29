3fd6ee9f6e35d7a61dd65427b4f1982756986b9c
Peter Khalil <rikokamil97@gmail.com>
2024-02-29 04:12:41 +0200

    removes semi

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import app from './app';
-import { Request, Response, NextFunction } from 'express';
+import app from './app'
+import { Request, Response, NextFunction } from 'express'
 const auth = async (req: Request, res: Response, next: NextFunction) => {
     if (req.body.username === 'admin' && req.body.password === 'admin') {
-        next();
+        next()
     } else {
-        return res.status(401).send('Unauthorized');
+        return res.status(401).send('Unauthorized')
     }
-};
+}
 
 app.get('/user', (req: Request, res: Response) => {
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    res.json(`Welcome user ip:${ip}`);
-});
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+    res.json(`Welcome user ip:${ip}`)
+})
 app.post('/login', (req: Request, res: Response) => {
-    res.json('Login successful');
-});
-app.use(auth);
+    res.json('Login successful')
+})
+app.use(auth)
 app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome to my API');
-});
+    res.send('Welcome to my API')
+})
